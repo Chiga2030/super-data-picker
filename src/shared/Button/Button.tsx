aa6a3f6e866732cd7d063ea84535588ffe40ab3b
Anton Tkachuk <tkachuk.ant@mail.ru>
2023-12-28 02:10:02 +0300

    added variant for shared button

--- src/shared/Button/Button.tsx
+++ src/shared/Button/Button.tsx
@@ -1,18 +1,31 @@
 import { DetailedHTMLProps, HtmlHTMLAttributes, ReactNode, } from 'react'
 import styles from './Button.module.scss'
+import { clsx, } from 'clsx'
 
 
 interface Props extends DetailedHTMLProps<HtmlHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
   children: ReactNode
+  variant?: 'default' | 'link'
 }
 
 
 export const Button = ({
   children,
+  variant = 'default',
   ...restProps
 }: Props): JSX.Element => {
   return (
-    <button className={ styles.button } type={ 'button' } { ...restProps }>
+    <button
+      className={ clsx(
+        styles.button,
+        {
+          [ styles.button_default ]: variant === 'default',
+          [ styles.button_link ]: variant === 'link',
+        }
+      ) }
+      type={ 'button' }
+      { ...restProps }
+    >
       { children }
     </button>
   )
