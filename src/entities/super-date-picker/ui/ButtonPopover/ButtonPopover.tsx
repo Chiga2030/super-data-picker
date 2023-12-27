3df366fcb0b5f3f7c841e96f01181bfeb87ce072
Anton Tkachuk <tkachuk.ant@mail.ru>
2023-12-27 22:23:47 +0300

    popover content rebased like a prop

--- src/entities/super-date-picker/ui/ButtonPopover/ButtonPopover.tsx
+++ src/entities/super-date-picker/ui/ButtonPopover/ButtonPopover.tsx
@@ -1,20 +1,17 @@
 import { Dispatch, ReactNode, SetStateAction, useEffect, useRef, useState, } from 'react'
 import styles from './ButtonPopover.module.scss'
 import { clsx, } from 'clsx'
-import { Tabs, } from '../../../tabs'
-import { AbsoluteDataInput, } from '../AbsoluteDataInput/AbsoluteDataInput'
-import { RelativeDateInput, } from '../RelativeDateInput/RelativeDateInput'
 
 
 interface Props {
   children: (setIsOpen: Dispatch<SetStateAction<boolean>>) => ReactNode
-  changeDateCallback: (value: string) => void
+  popoverContent: ReactNode
 }
 
 
 export const ButtonPopover = ({
   children,
-  changeDateCallback,
+  popoverContent,
 }: Props): JSX.Element => {
   const [ isOpen, setIsOpen, ] = useState(false)
   const popoverRef = useRef(null)
@@ -43,25 +40,10 @@ export const ButtonPopover = ({
   }, [ isOpen, ])
 
 
-  const tabList = [ {
-    key: 1,
-    title: 'Абсолютная дата',
-    content: (
-      <AbsoluteDataInput changeDateCallback={ changeDateCallback } />
-    ),
-  }, {
-    key: 2,
-    title: 'Относительная дата',
-    content: (
-      <RelativeDateInput changeDateCallback={ changeDateCallback } />
-    ),
-  }, ]
-
-
   return (
     <div ref={ popoverRef }>
       <div className={ clsx(styles.popover, { [ styles.popover_open ]: isOpen, }) } >
-        <Tabs tabList={ tabList } />
+        { popoverContent }
       </div>
 
       { children(setIsOpen) }
--- src/entities/super-date-picker/ui/SelectStartDateButton/SelectStartDateButton.tsx
+++ src/entities/super-date-picker/ui/SelectStartDateButton/SelectStartDateButton.tsx
@@ -1,6 +1,10 @@
 import { ReactNode, SetStateAction, } from 'react'
 import { ButtonPopover, } from '../ButtonPopover/ButtonPopover'
 import styles from './SelectStartDateButton.module.scss'
+import { Button, } from '../../../../shared/Button'
+import { Tabs, } from '../../../tabs'
+import { AbsoluteDataInput, } from '../AbsoluteDataInput/AbsoluteDataInput'
+import { RelativeDateInput, } from '../RelativeDateInput/RelativeDateInput'
 
 
 interface Props {
@@ -13,6 +17,21 @@ export const SelectStartDateButton = ({
   children,
   changeDateCallback,
 }: Props): JSX.Element => {
+  const tabList = [ {
+    key: 1,
+    title: 'Абсолютная дата',
+    content: (
+      <AbsoluteDataInput changeDateCallback={ changeDateCallback } />
+    ),
+  }, {
+    key: 2,
+    title: 'Относительная дата',
+    content: (
+      <RelativeDateInput changeDateCallback={ changeDateCallback } />
+    ),
+  }, ]
+
+
   const handleClick = (setIsOpen: (value: SetStateAction<boolean>) => void) => {
     setIsOpen(prev => !prev)
   }
@@ -20,11 +39,11 @@ export const SelectStartDateButton = ({
 
   return (
     <div className={ styles.wrapper }>
-      <ButtonPopover changeDateCallback={ changeDateCallback }>
+      <ButtonPopover popoverContent={ <Tabs tabList={ tabList } /> }>
         { setIsOpen => (
-          <button type={ 'button' } onClick={ () => handleClick(setIsOpen) }>
+          <Button onClick={ () => handleClick(setIsOpen) }>
             { children }
-          </button>
+          </Button>
         ) }
       </ButtonPopover>
     </div>
