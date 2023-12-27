64c541ec30c9af51d3e6b8eda76ef592d4869c12
Anton Tkachuk <tkachuk.ant@mail.ru>
2023-12-28 00:49:11 +0300

    added context to picker component

--- src/entities/super-date-picker/SuperDatePicker.tsx
+++ src/entities/super-date-picker/SuperDatePicker.tsx
@@ -1,5 +1,9 @@
+import { useContext, } from 'react'
 import styles from './SuperDatePicker.module.scss'
+import { QuickSelectButton, } from './ui/QuickSelectButton/QuickSelectButton'
 import { SelectStartDateButton, } from './ui/SelectStartDateButton/SelectStartDateButton'
+import dayjs from 'dayjs'
+import { ContextProvider, EndDateContext, StartDateContext, } from './model/ContextProvider/ContextProvider'
 
 
 interface Props {
@@ -8,16 +12,47 @@ interface Props {
 }
 
 
-export const SuperDatePicker = ({
+const SuperDatePickerBox = ({
   changeStartDateCallback,
   changeEndDateCallback,
 }: Props): JSX.Element => {
+  const { date: startDate, setDate: setStartDate, } = useContext(StartDateContext)
+  const { date: endDate, setDate: setEndDate, } = useContext(EndDateContext)
+
+  const handleChangeStartDate = (value: string) => {
+    changeStartDateCallback(value)
+    if (setStartDate) {
+      setStartDate(dayjs(value).format('DD.MM.YYYY HH:mm'))
+    }
+  }
+
+  const handleChangeEndDate = (value: string) => {
+    changeEndDateCallback(value)
+    if (setEndDate) {
+      setEndDate(dayjs(value).format('DD.MM.YYYY HH:mm'))
+    }
+  }
+
+
   return (
-    <>
-      <div className={ styles.wrapper }>
-        <SelectStartDateButton changeDateCallback={ changeStartDateCallback }>Выбрать дату начала</SelectStartDateButton>
-        <SelectStartDateButton changeDateCallback={ changeEndDateCallback }>Выбрать дату окончания</SelectStartDateButton>
-      </div>
-    </>
+    <div className={ styles.wrapper }>
+      <QuickSelectButton />
+      <SelectStartDateButton changeDateCallback={ handleChangeStartDate }>
+        { startDate ? startDate : 'Выбрать дату начала' }
+      </SelectStartDateButton>
+
+      <SelectStartDateButton changeDateCallback={ handleChangeEndDate }>
+        { endDate ? endDate : 'Выбрать дату окончания' }
+      </SelectStartDateButton>
+    </div>
+  )
+}
+
+
+export const SuperDatePicker = ({ ...props }: Props) => {
+  return (
+    <ContextProvider>
+      <SuperDatePickerBox { ...props } />
+    </ContextProvider>
   )
 }
